10e8eb9c06f60105bc24ff62dc982451df821c74
Bjorn Blomberg <Brasse.bb@gmail.com>
2024-04-02 14:47:13 +0200

    added ebiten and got image render working

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # GoTowerDefense
 a Tower Defense with the inspiriation from Plants Vs Zombis
+
+Using 2D libb ebitengine
+https://ebitengine.org/en/documents/install.html
+
+## install
+´´´
+$sudo dnf install mesa-libGL-devel mesa-libGLES-devel libXrandr-devel libXcursor-devel libXinerama-devel libXi-devel libXxf86vm-devel alsa-lib-devel pkg-config
+´´´
+### test if all works
+'''
+go run github.com/hajimehoshi/ebiten/v2/examples/rotate@latest
+'''
+### for windows
+'''
+GOOS=windows go run github.com/hajimehoshi/ebiten/v2/examples/rotate@latest
+'''
--- main.go
+++ main.go
@@ -4,7 +4,49 @@ import (
 	"fmt"
 	"time"
 	"GoTowerDefense/objekts"
+	"log"
+	_ "image/png"
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
+
+var img *ebiten.Image
+
+func init() {
+	var err error
+	img, _, err = ebitenutil.NewImageFromFile("gopher.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+type Game struct{
+	GWorld *GameWorld
+}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	ebitenutil.DebugPrint(screen, "Hello, World!")
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Scale(1, 1)
+	op.GeoM.Translate(float64(g.GWorld.Entatys[0].Posison.Y), float64(g.GWorld.Entatys[0].Posison.X))
+	screen.DrawImage(img, op)
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return 320, 240
+}
+
+func runGui(gWorld *GameWorld) {
+	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowTitle("Hello, World!")
+	if err := ebiten.RunGame(&Game{GWorld: gWorld}); err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
 	//test := objekts.Vektor{X: 0,Y: 0}
@@ -15,6 +57,7 @@ func main() {
 	fmt.Println(gWorld)
 	//fmt.Println(player)
 	ticker := time.NewTicker(1 * time.Second)
+	go runGui(gWorld)
 	go loop(ticker, gWorld)
 	inputLoop(ticker, player)
 }
